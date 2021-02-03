9804067ca73eef32ba0bddc29af4748f74a8753a
Rafael Guimarães <rafael_guiimaraes@hotmail.com>
2021-02-03 19:08:29 -0300

    criado o formulario do post

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Formik } from "formik";
+import TextField from "@material-ui/core/TextField";
+import { Container, ContentForm } from "./styles";
+import * as Yup from "yup";
 
-// import { Container } from './styles';
+const FormSchema = Yup.object().shape({
+  title: Yup.string().required(),
+  body: Yup.string().required(),
+});
 
 const PostForm = () => {
-  return <div>Post</div>;
+  const [initialValues] = useState({
+    title: "",
+    body: "",
+  });
+
+  return (
+    <Container>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => console.log("valuesss", values)}
+        validationSchema={FormSchema}
+      >
+        {({ handleChange, errors, handleSubmit, values }) => (
+          <ContentForm>
+            <TextField
+              label="title"
+              type="text"
+              value={values.title}
+              onChange={handleChange("title")}
+              variant="outlined"
+            />
+            {errors.title && <h3>{errors.title}</h3>}
+
+            <TextField
+              label="Body"
+              type="text"
+              value={values.body}
+              onChange={handleChange("body")}
+              multiline
+              rows={4}
+              variant="outlined"
+            />
+            {errors.body && <h3>{errors.body}</h3>}
+
+            <button onClick={handleSubmit}>salvar</button>
+          </ContentForm>
+        )}
+      </Formik>
+    </Container>
+  );
 };
 
 export default PostForm;
---
+++ src/components/PostForm/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const ContentForm = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
